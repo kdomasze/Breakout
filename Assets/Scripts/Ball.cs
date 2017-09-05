ddb76051def7db6450658cde450d6cf015569884
Kyle Domaszewicz <kyledomaszewicz@gmail.com>
2017-09-05 16:40:59 -0700

    Adds padal and basic input logic

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+    public float IntialSpeed = 5;
     private Rigidbody2D _rigidbody;
 
 	// Use this for initialization
 	void Start ()
 	{
 	    _rigidbody = GetComponent<Rigidbody2D>();
-        _rigidbody.velocity = new Vector3(10, 10, 0);
+        _rigidbody.velocity = new Vector3(IntialSpeed, IntialSpeed, 0);
 	}
 	
 	// Update is called once per frame
---
+++ Assets/Scripts/Padal.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Padal : MonoBehaviour
+{
+    public float Speed = 5;
+
+    private Transform _tranform;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    _tranform = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    float input = Input.GetAxisRaw("Horizontal");
+
+        _tranform.Translate(new Vector3(input * Speed * Time.deltaTime, 0, 0));
+	}
+}
