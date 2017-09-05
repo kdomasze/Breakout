d8ba63ccd1911aa20be7016c0bdc54b5f15787f1
Kyle Domaszewicz <kyledomaszewicz@gmail.com>
2017-09-05 16:17:31 -0700

    Creates the basic scene to test ball physics

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    _rigidbody = GetComponent<Rigidbody2D>();
+        _rigidbody.velocity = new Vector3(10, 10, 0);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
