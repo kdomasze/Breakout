9c48c94e99f36a86ec6e52e8e57c209b1724b171
Kyle Domaszewicz <kyledomaszewicz@gmail.com>
2017-09-05 18:19:13 -0700

    Adds game over zone that resets the position of the ball

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -5,13 +5,17 @@ using UnityEngine;
 public class Ball : MonoBehaviour
 {
     public float IntialSpeed = 5;
+
     private Rigidbody2D _rigidbody;
+    private Transform _transform;
 
 	// Use this for initialization
 	void Start ()
 	{
 	    _rigidbody = GetComponent<Rigidbody2D>();
-        _rigidbody.velocity = new Vector3(IntialSpeed, IntialSpeed, 0);
+	    _transform = GetComponent<Transform>();
+
+        Reset();
 	}
 	
 	// Update is called once per frame
@@ -20,13 +24,29 @@ public class Ball : MonoBehaviour
 		
 	}
 
+    void Reset()
+    {
+        _transform.position = new Vector3(0, 0);
+        _rigidbody.velocity = new Vector3(IntialSpeed, IntialSpeed, 0);
+    }
+
     void OnCollisionEnter2D(Collision2D col)
     {
-        Debug.Log(col.gameObject.tag);
+        Debug.Log("Collided with: " + col.gameObject.tag);
 
         if (col.gameObject.CompareTag("Piece"))
         {
             col.gameObject.GetComponent<Piece>().OnHit();
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Triggered on: " + other.gameObject.tag);
+
+        if (other.gameObject.CompareTag("Game Over"))
+        {
+            this.Reset();
         }
     }
 }
