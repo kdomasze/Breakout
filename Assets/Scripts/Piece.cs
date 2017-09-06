21514c50e18fc696f85830b4ec828a0f0521ddee
Kyle Domaszewicz <kyledomaszewicz@gmail.com>
2017-09-06 14:46:01 -0700

    Adds score manager and UI for displaying the score

--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -1,11 +1,14 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Piece : MonoBehaviour
 {
+    public static Action OnDestroy; 
     public void OnHit()
     {
+        OnDestroy.Invoke();
         Destroy(gameObject);
     }
 }
---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    public Text ScoreText;
+    public int PointsPerBrick;
+    private int score = 0;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    Piece.OnDestroy += () => score += PointsPerBrick;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    ScoreText.text = "Score: " + score;
+	}
+}
